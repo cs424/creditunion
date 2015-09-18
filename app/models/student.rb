c325e610a0d0c632cab3d1c1249fc32c132d9131
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2015-09-18 10:01:26 +0100

    added to_s method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ActiveRecord::Base
   validate :number, :first, :last, :username, presence: true
   validate :number, :username, uniqueness: true
+
+  def to_s
+    "#{number}: #{last}, #{first}"
+  end
 end
