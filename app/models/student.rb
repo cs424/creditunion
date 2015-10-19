470b916f00afbb10ab07f38ff1539e6ea436d8c9
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2015-10-19 17:53:42 +0100

    modified student views

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,8 @@ class Student < ActiveRecord::Base
   validate :number, :first, :last, :username, presence: true
   validate :number, :username, uniqueness: true
 
+  default_scope { order(:last, :first) }
+
   def to_s
     "#{number}: #{last}, #{first}"
   end
